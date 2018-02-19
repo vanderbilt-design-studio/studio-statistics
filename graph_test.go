0a0c293e546cf1ffe81d963bb56987f1191bc199
Sameer <spuri4096@gmail.com>
2018-02-18 19:46:00 -0600

    Add test

---
+++ graph_test.go
@@ -0,0 +1,13 @@
+package studio_statistics
+
+import (
+    "testing"
+    "os"
+)
+func TestMakeGraph(t *testing.T) {
+	in, _ := os.Open("activity.log")
+	out, _ := os.Create("graph.png")
+	if err := MakeGraph(in, out); err != nil {
+		t.Error(err)
+	}
+}
